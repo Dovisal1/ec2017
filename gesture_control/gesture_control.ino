5df610bf8f35a0942251938bf8a8d14747a417e7
Dovi Salomon <Dovisal1@gmail.com>
2017-11-22 00:15:09 -0500

    reinitalize the DMP in case of freeze

--- gesture_control/gesture_control.ino
+++ gesture_control/gesture_control.ino
@@ -28,45 +28,54 @@ uint8_t fifo[64];
 Quaternion q;
 VectorFloat gravity;
 float ypr[3]; // yaw, pitch, roll
+unsigned long lastread = 0;
 
 void setup() {
+
+  // Begin the I2C for MPU6050 (gyroscope input)
 	Wire.begin();
 	TWBR = 24; //400KHz SCL
 
+  // initialize the external interrupt on pin2
   EICRA = 0x03;
   EIMSK = 0x01;
 
-	Serial.begin(115200);
+	Serial.begin(9600);
 
 	mpu.initialize();
-	//mpu.testConnection();
+ 
+	Serial.print(F("Testing device connections..."));
 
-	uint8_t dmpstatus = mpu.dmpInitialize();
+  while (!mpu.testConnection()) {
+    Serial.println(F("MPU6050 connection failed."));
+    Serial.print(F("Trying again..."));
+    mpu.initialize();
+    delay(250);
+  }
+
+  Serial.println(F("MPU6050 connection successful."));
 
 	mpu.setXGyroOffset(49);
 	mpu.setYGyroOffset(-15);
 	mpu.setZGyroOffset(49);
 	mpu.setZAccelOffset(1546);
 
-	if (!dmpstatus) { //success
-		mpu.setDMPEnabled(true);
-		mpu.getIntStatus();
-
-		mpuIntStatus = mpu.getIntStatus();
-
-		packet_size = mpu.dmpGetFIFOPacketSize();
-	} // else error
+	while (mpu.dmpInitialize())
+    Serial.println(F("DMP init failed!"));
+  
+  mpu.setDMPEnabled(true);
+  packet_size = mpu.dmpGetFIFOPacketSize();
 
   radio.begin();
   radio.openWritingPipe(addresses[1]);
-  radio.openReadingPipe(1, addresses[0]);
-  
+  radio.openReadingPipe(1, addresses[0]); 
 }
 
 void loop() {
-
+  
 	if (mpuInterrupt || fifo_count >= packet_size) {
 		mpuInterrupt = false;
+    lastread = millis();
 		mpuIntStatus = mpu.getIntStatus();
 		fifo_count = mpu.getFIFOCount();
 
@@ -90,13 +99,19 @@ void loop() {
 			ypr[1] = ypr[1] * 180/M_PI;
 			ypr[2] = ypr[2] * 180/M_PI;
 
-      if (!radio.write(ypr+1, 2*sizeof(float))) {
-        Serial.println("tx error");
-      }
+      if (!radio.write(ypr+1, 2*sizeof(float)))
+        //Serial.println(F("tx error"));
+        ;
 
       Serial.print(ypr[1]);
-      Serial.print(" ");
+      Serial.print(F(" "));
       Serial.println(ypr[2]);
 		}
+	} else if (millis() - lastread > 250) {
+    Serial.println(F("timeout!"));
+    while (mpu.dmpInitialize())
+      ;
+    mpu.setDMPEnabled(true);
+    lastread = millis();
 	}
 }
