0fcea2aa0d0356da0c4f6e7edb5551f0e06256db
Dov Salomon <dovisal1@gmail.com>
2017-12-21 16:50:37 -0500

    lots of comments and reformatting

--- drive_control/drive_control.ino
+++ drive_control/drive_control.ino
@@ -2,29 +2,50 @@
 #include <SPI.h>
 #include "RF24.h"
 
-byte addresses[][7] = {"3drive", "3cntrl"};
 RF24 radio(7, 8);
+byte addresses[][7] = {"3drive", "3cntrl"};
 
+// set DEBUG to 1 for serial status updates when testing
 #define DEBUG 0
 #define Dprint(s) if (DEBUG) Serial.print(s)
 #define Dprintln(s) if (DEBUG) Serial.println(s)
 
-void setup() {
+void setup()
+{
+  /** PWM motor driver setup
+   *
+   * Phase Correct PWM mode with ICR as top
+   * clock prescaler = 8
+   * ICR1 (top) = 20000
+   * so f = 16Mhz / 8 / 20000 / 2 = 50Hz
+   * OCR1A and OCR1B control the duty cycle on PB1 and PB2
+   * 
+   * each clock tick is 1us, so OCR1A and OCR1B should be
+   * on the range 1000 to 2000 for 1ms to 2ms pulses
+   * 1000: max reverse
+   * 1500: stop
+   * 2000: max forward
+   * (in practice we found that the motors were not so precise,
+   * and going a little above 2000 or below 1000 produces faster
+   * speeds)
+   */
 
-  // PWM out setup
   TCCR1A = 0xA2;
   TCCR1B = 0x12;
   TCCR1C = 0x00;
   ICR1 = 20000; // top
-  OCR1A = 1500; //right
-  OCR1B = 1500; //left
+
+  // initialize the motors to off
+  OCR1A = 1500; // right motor
+  OCR1B = 1500; // left motor
   
+  // enable the PWM output on PB1 and PB2
   DDRB = 0x06;
 
   #if DEBUG == 1
-    Serial.begin(9600);
-    while(!Serial)
-      ;
+  Serial.begin(9600);
+  while(!Serial)
+    ;
   #endif
 
   // Radio Communication Setup
@@ -36,10 +57,11 @@ void setup() {
 
 unsigned long last_rx = 0;
 
+// dynamic range 
 float pmax = 55;
 float rmax = 55;
 
-
+// parameters for the 
 #define TURN_THRESHOLD      15
 #define TURN_CURVE          4.0
 #define THROTTLE_THRESHOLD  10
@@ -50,8 +72,8 @@ float rmax = 55;
 #define TIMEOUT             500
 
 
-void loop() {
-  
+void loop()
+{  
   float p, r, pr[2]; // pitch roll values
 
   float cap;
@@ -61,82 +83,106 @@ void loop() {
   
   if (radio.available()) {
 
-      while(radio.available()) {
-        radio.read(pr, sizeof(pr));
-      }
-      
-      p = pr[0];
-      r = pr[1];
+    while(radio.available()) {
+      radio.read(pr, sizeof(pr));
+    }
 
-      speed = abs(p);
-      
-      if (speed > pmax)
-        pmax = speed;
-      if (abs(r) > rmax)
-        rmax = abs(r);
+    p = pr[0]; // pitch (throttle)
+    r = pr[1]; // roll  (steering)
 
-      /* turning logic */
-      if (r > TURN_THRESHOLD) { //left turn
-        cap = polymap(r, TURN_THRESHOLD, rmax, 0.0, 100.0, TURN_CURVE);
-        rpercent = (300.0 - cap) / 200.0;
-        lpercent = (100.0 - cap) / 100.0;
-      } else if (r < -TURN_THRESHOLD) { //right turn
-        cap = polymap(r, -TURN_THRESHOLD, -rmax, 0.0, 100.0, TURN_CURVE);
-        lpercent = (300.0 - cap) / 200.0;
-        rpercent = (100.0 - cap) / 100.0;
-      } else {
-        lpercent = rpercent = 1.0;
-      }
+    speed = abs(p);
 
-      /* speed mapping */
-      if (speed > THROTTLE_MAX)
-        diff = THROTTLE_TOP;
-      else if (speed > THROTTLE_THRESHOLD)
-        diff = polymap(speed, THROTTLE_THRESHOLD, THROTTLE_MAX, THROTTLE_STOP, THROTTLE_TOP, THROTTLE_CURVE);
-      else
-        diff = map(speed, 0, THROTTLE_THRESHOLD, 0, THROTTLE_STOP);
+    if (speed > pmax)
+      pmax = speed;
+    if (abs(r) > rmax)
+      rmax = abs(r);
 
-      /* apportioning speed to each wheel based on turn */
-      rdiff = diff * rpercent;
-      ldiff = diff * lpercent;
+    /** speed mapping
+     * There are 3 regions:
+     *  - if the pitch is above a top threshold the maximum speed is assigned.
+     *  - if the pitch is below a bottom threshold the speed is zero.
+     *  - otherwise, the speed is non-linearly mapped to somewhere in between.
+     */
+    if (speed > THROTTLE_MAX)
+      diff = THROTTLE_TOP;
+    else if (speed > THROTTLE_THRESHOLD)
+      diff = polymap(speed, THROTTLE_THRESHOLD, THROTTLE_MAX, THROTTLE_STOP, THROTTLE_TOP, THROTTLE_CURVE);
+    else
+      diff = map(speed, 0, THROTTLE_THRESHOLD, 0, THROTTLE_STOP);
 
-      /* adjusting for speed limit on each wheel */
-      if (rdiff > THROTTLE_TOP) {
-          ldiff -= (rdiff - THROTTLE_TOP);
-          rdiff = THROTTLE_TOP;
-      } else if (ldiff > THROTTLE_TOP) {
-        rdiff -= (ldiff - THROTTLE_TOP);
-        ldiff = THROTTLE_TOP;
-      }
+    /** turning logic
+     * if the roll is above the threshold, in either direction,
+     * a turn is detected
+     *
+     * The roll is converted non-linearly to a number from 0 to 100.
+     * This number is then used to create a percentage for each wheel
+     *  - the outer wheel is sped up, anywhere from 100% to 150% of original speed.
+     *  - the inner wheel is slowed down, anywhere from 0% to 100% of original speed
+     */
+    if (r > TURN_THRESHOLD)
+    { //left turn
+      cap = polymap(r, TURN_THRESHOLD, rmax, 0.0, 100.0, TURN_CURVE);
+      rpercent = (300.0 - cap) / 200.0;
+      lpercent = (100.0 - cap) / 100.0;
+    }
+    else if (r < -TURN_THRESHOLD)
+    { //right turn
+      cap = polymap(r, -TURN_THRESHOLD, -rmax, 0.0, 100.0, TURN_CURVE);
+      lpercent = (300.0 - cap) / 200.0;
+      rpercent = (100.0 - cap) / 100.0;
+    }
+    else
+    { // no turn
+      // equal weight for each wheel
+      lpercent = rpercent = 1.0;
+    }
 
-      /* invert the pulse differential from the mean if going in reverse */
-      if (p < 0) {
-        rdiff = -rdiff;
-        ldiff = -ldiff;
-      }
+    // apportioning speed to each wheel based on turn
+    rdiff = diff * rpercent;
+    ldiff = diff * lpercent;
 
-      /* update the pwm motor output */
-      OCR1A = 1500 + rdiff;
-      OCR1B = 1500 + ldiff;
+    // adjusting for speed limit on each wheel in case we went over
+    if (rdiff > THROTTLE_TOP) {
+      ldiff -= (rdiff - THROTTLE_TOP);
+      rdiff = THROTTLE_TOP;
+    } else if (ldiff > THROTTLE_TOP) {
+      rdiff -= (ldiff - THROTTLE_TOP);
+      ldiff = THROTTLE_TOP;
+    }
 
-      #if DEBUG == 1
-        char fmt[] = "p = %s; r = %s; diff = %d; OCR1A = %d; OCR1B = %d\n";
-        char buf[256];
-        char rbuf[16], pbuf[16];
-        dtostrf(p, 4, 2, pbuf);
-        dtostrf(r, 4, 2, rbuf);
-        sprintf(buf, fmt, pbuf, rbuf, diff, OCR1A, OCR1B);
-        Serial.println(buf);
-      #endif
+    // invert the pulse differential from the mean if going in reverse
+    if (p < 0) {
+      rdiff = -rdiff;
+      ldiff = -ldiff;
+    }
 
-      last_rx = millis();
-  } else if (millis() - last_rx > TIMEOUT) {
+    // update the motors
+    OCR1A = 1500 + rdiff;
+    OCR1B = 1500 + ldiff;
+
+    #if DEBUG == 1
+      char fmt[] = "p = %s; r = %s; diff = %d; OCR1A = %d; OCR1B = %d\n";
+      char buf[256];
+      char rbuf[16], pbuf[16];
+      dtostrf(p, 4, 2, pbuf);
+      dtostrf(r, 4, 2, rbuf);
+      sprintf(buf, fmt, pbuf, rbuf, diff, OCR1A, OCR1B);
+      Serial.println(buf);
+    #endif
+
+    // record the transmission time
+    last_rx = millis();
+  }
+  // if no transmission received recently then stop moving
+  else if (millis() - last_rx > TIMEOUT)
+  {
     OCR1A = OCR1B = 1500; // stop motors
     last_rx = millis();
   }
 }
 
+// provides a non-linear mapping (if curve != 1)
 static inline float polymap(float x, float imn, float imx, float omn, float omx, float curve) {
-    return omn + (omx - omn) * pow((x - imn) / (imx - imn), curve);
+  return omn + (omx - omn) * pow((x - imn) / (imx - imn), curve);
 }
 
--- gesture_control/gesture_control.ino
+++ gesture_control/gesture_control.ino
@@ -9,36 +9,40 @@
 #include <SPI.h>
 #include "RF24.h"
 
+// set DEBUG to 1 for to get status ouputs when testing
 #define DEBUG 0
 #define Dprint(s) if (DEBUG) Serial.print(s)
 #define Dprintln(s) if (DEBUG) Serial.println(s)
 
+// radio object
+RF24 radio(7,8);
 byte addresses[][7] = {"3drive", "3cntrl"};
 
-RF24 radio(7,8);
-
+// gyroscope object
 MPU6050 mpu;
 
+// external interrupt on pin 2 for gyroscope
 volatile bool mpuInterrupt = false;
 ISR(INT0_vect) {
-	mpuInterrupt = true;
+  mpuInterrupt = true;
 }
 
+// gyro status and data variables
 uint8_t mpuIntStatus;
 uint16_t packet_size;
 uint16_t fifo_count;
 uint8_t fifo[64];
-
 Quaternion q;
 VectorFloat gravity;
 float pr[2]; // pitch, roll
+
 unsigned long lastread = 0;
 
-void setup() {
-  
-  // Begin the I2C for MPU6050 (gyroscope input)
-	Wire.begin();
-	TWBR = 24; //400KHz SCL
+void setup()
+{  
+  // Begin the I2C
+  Wire.begin();
+  TWBR = 24; //400KHz SCL
 
   // initialize the external interrupt on pin2
   EICRA = 0x03;
@@ -50,9 +54,9 @@ void setup() {
       ;
   #endif
 
-	mpu.initialize();
+  mpu.initialize();
  
-	Dprint(F("Testing device connections..."));
+  Dprint(F("Testing device connections..."));
 
   while (!mpu.testConnection()) {
     Dprintln(F("MPU6050 connection failed."));
@@ -63,12 +67,12 @@ void setup() {
 
   Dprintln(F("MPU6050 connection successful."));
 
-	mpu.setXGyroOffset(49);
-	mpu.setYGyroOffset(-15);
-	mpu.setZGyroOffset(49);
-	mpu.setZAccelOffset(1546);
+  mpu.setXGyroOffset(49);
+  mpu.setYGyroOffset(-15);
+  mpu.setZGyroOffset(49);
+  mpu.setZAccelOffset(1546);
 
-	while (mpu.dmpInitialize())
+  while (mpu.dmpInitialize())
     Dprintln(F("DMP init failed!"));
   
   mpu.setDMPEnabled(true);
@@ -79,48 +83,54 @@ void setup() {
   radio.openReadingPipe(1, addresses[0]);
 }
 
-void loop() {
+void loop()
+{
 
-  wdt_reset(); // update the watchdog
-  
-	if (mpuInterrupt || fifo_count >= packet_size) {
-		mpuInterrupt = false;
-		mpuIntStatus = mpu.getIntStatus();
-		fifo_count = mpu.getFIFOCount();
+  if (mpuInterrupt || fifo_count >= packet_size)
+  {
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+    fifo_count = mpu.getFIFOCount();
 
-		// check for FIFO overflow
-		if ((mpuIntStatus & 0x10) || fifo_count == 1024) {
-			mpu.resetFIFO();
-			// otherwise, check for DMP ready
-		} else if (mpuIntStatus & 0x02) {
-			while(fifo_count < packet_size)
-				fifo_count = mpu.getFIFOCount();
+    // check for FIFO overflow
+    if ((mpuIntStatus & 0x10) || fifo_count == 1024) {
+      mpu.resetFIFO();
+    }
+    // otherwise, check for DMP ready
+    else if (mpuIntStatus & 0x02) {
+      while(fifo_count < packet_size)
+        fifo_count = mpu.getFIFOCount();
 
-			mpu.getFIFOBytes(fifo, packet_size);
-			fifo_count -= packet_size;
+      mpu.getFIFOBytes(fifo, packet_size);
+      fifo_count -= packet_size;
 
-			// get values
-			mpu.dmpGetQuaternion(&q, fifo);
-			mpu.dmpGetGravity(&gravity, &q);
+      // get values
+      mpu.dmpGetQuaternion(&q, fifo);
+      mpu.dmpGetGravity(&gravity, &q);
       mpu.dmpGetPitchRoll(pr, &q, &gravity);
 
-			// convert pitch and roll to degrees
-			pr[0] = pr[0] * 180/M_PI;
-			pr[1] = pr[1] * 180/M_PI;
+      // convert pitch and roll to degrees
+      pr[0] = pr[0] * 180/M_PI;
+      pr[1] = pr[1] * 180/M_PI;
 
+      // transmit the orientation angles
       radio.write(pr, sizeof(pr));
 
       Dprint(pr[0]);
       Dprint(F(" "));
       Dprintln(pr[1]);
-		}
+    }
    
+   // record the last gyro read
    lastread = millis();
-	} else if (millis() - lastread > 250) {
+  }
+  // if no gyro read in last 250ms restart it
+  else if (millis() - lastread > 250)
+  {
     Dprintln(F("timeout!"));
     while (mpu.dmpInitialize())
       Dprintln(F("init"));
     mpu.setDMPEnabled(true);
     lastread = millis();
-	}
+  }
 }
